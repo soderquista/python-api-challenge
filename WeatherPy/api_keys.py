0a53b7ab89f6946c7f5ef2c9cb7c77580ad403aa
soderquista <e.alex.sod@gmail.com>
2023-02-01 23:25:08 -0800

    Project complete

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "cff37b53c8d63d54739a4d2029ad8540"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "229654705af94e60b5a8849c44d40a0f"
